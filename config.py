7ad4857492db263dc05f04bc5851a6f9aa4c75a3
Lee Chun Kai <78149270+BananaKing123@users.noreply.github.com>
2023-09-09 21:24:41 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cnujoegmoltx.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leechunkai-employee"
+customregion = "us-east-1"
+
